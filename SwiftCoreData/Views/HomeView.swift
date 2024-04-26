85b1078917334a8b6f0911817a7c5d674af9bc67
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-04-26 15:57:45 -0400

    ADDED: create new entities with core data. FIXED: reordering of view and base directories

--- SwiftCoreData/Views/HomeView.swift
+++ SwiftCoreData/Views/HomeView.swift
@@ -1,17 +1,14 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  SwiftCoreData
 //
 //  Created by Enrique Sarmiento on 26/4/24.
 //
 
 import SwiftUI
-import CoreData
 
-struct ContentView: View {
-
+struct HomeView: View {
     var body: some View {
-       HomeView()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-
 }
